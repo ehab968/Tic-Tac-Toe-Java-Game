6c1b32fe51ee161808760029cbe606226f2cf702
ELDemy <mahmoudeldemerdash5@gmail.com>
2025-12-13 00:12:57 +0200

    draw line when winning

--- TicTacToeGui/src/main/java/com/mycompany/tictactoegui/PrimaryController.java
+++ TicTacToeGui/src/main/java/com/mycompany/tictactoegui/PrimaryController.java
@@ -11,6 +11,7 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Line;
 
 public class PrimaryController implements Initializable {
 
@@ -67,64 +68,17 @@ public class PrimaryController implements Initializable {
 
         int cellId = Integer.parseInt(cell.getId());
         char cellShape = gridInputs.get(cellId);
-        int shapeAlign = 1;
 
-        //check horizontal
-        int ptr = cellId;
-        while (ptr % 3 != 0) {// if ptr == 3,6,9 don't enter
-            ptr++;
-            if (gridInputs.get(ptr) != null && gridInputs.get(ptr) == cellShape) {
-                shapeAlign++;
-            } else {
-                break;
-            }
-        }
+        int[][] wins = {
+            //Horizontal
+            {1, 2, 3}, {4, 5, 6}, {7, 8, 9},
+            //Vertical
+            {1, 4, 7}, {2, 5, 8}, {3, 6, 9},
+            // diagonal
+            {1, 5, 9}, {3, 5, 7}
+        };
 
-        ptr = cellId - 1;
-        while (ptr % 3 != 0) {  // if ptr == 0,3,6,9 don't enter as cellId = 1,4,7
-            if (gridInputs.get(ptr) != null && gridInputs.get(ptr) == cellShape) {
-                shapeAlign++;
-            } else {
-                break;
-            }
-            ptr--;
-        }
-
-        if (shapeAlign == 3) {
-            userWon();
-            return;
-        }
-
-        //check vertical
-        shapeAlign = 1;
-        ptr = cellId + 3;
-        while (ptr <= 9) {
-            if (gridInputs.get(ptr) != null && gridInputs.get(ptr) == cellShape) {
-                shapeAlign++;
-            } else {
-                break;
-            }
-            ptr += 3;
-        }
-
-        ptr = cellId - 3;
-        while (ptr >= 1) {
-            if (gridInputs.get(ptr) != null && gridInputs.get(ptr) == cellShape) {
-                shapeAlign++;
-            } else {
-                break;
-            }
-            ptr -= 3;
-        }
-        if (shapeAlign == 3) {
-            userWon();
-            return;
-        }
-
-        //Check Diagonal
-        int[][] diagonalWins = {{1, 5, 9}, {3, 5, 7}};
-
-        for (int[] line : diagonalWins) {
+        for (int[] line : wins) {
             if (gridInputs.get(line[0]) != null
                     && gridInputs.get(line[1]) != null
                     && gridInputs.get(line[2]) != null
@@ -132,15 +86,62 @@ public class PrimaryController implements Initializable {
                     && gridInputs.get(line[1]) == cellShape
                     && gridInputs.get(line[2]) == cellShape) {
 
-                userWon();
+                userWon(line);
                 return;
             }
         }
 
     }
 
-    private void userWon() {
+    private void userWon(int[] line) {
+        drawWinLine(line);
         System.out.println("User " + currentPlayer + " won the Game!!!");
+    }
+
+    private void drawWinLine(int[] winCells) {
+        int first = winCells[0];
+        int last = winCells[2];
+
+        double[] start = getCellCenter(first);
+        double[] end = getCellCenter(last);
+
+        if (start == null || end == null) {
+            return;
+        }
+        Line line = new Line();
+        line.setStartX(start[0]);
+        line.setStartY(start[1]);
+        line.setEndX(end[0]);
+        line.setEndY(end[1]);
+
+        line.setStrokeWidth(6);
+        line.setStyle("-fx-stroke: red;");
+
+        gamePane.getChildren().add(line);
+    }
+
+    private double[] getCellCenter(int cellId) {
+        gridPane.applyCss();
+        gridPane.layout();
+        for (Node n : gridPane.getChildren()) {
+            if (!(n instanceof StackPane)) {
+                continue;
+            }
+
+            if (n.getId() != null && Integer.parseInt(n.getId()) == cellId) {
+                System.out.println(n);
+                // Get center in local coordinates
+                double centerX = n.getBoundsInLocal().getWidth() / 2;
+                double centerY = n.getBoundsInLocal().getHeight() / 2;
+                System.out.println("centerX = " + centerX + "centerY = " + centerY);
+                // Convert to gamePane coordinates
+                javafx.geometry.Point2D p = n.localToScene(centerX, centerY);
+                javafx.geometry.Point2D pInGamePane = gamePane.sceneToLocal(p);
+
+                return new double[]{pInGamePane.getX(), pInGamePane.getY()};
+            }
+        }
+        return null;
     }
 
 }
