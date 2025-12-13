27104acd2a211715e0f1770bb67def16197dca9f
Ahmed Sayed <149809000+ahmedSayed321@users.noreply.github.com>
2025-12-13 10:36:37 +0200

    implemnting grid ui

--- TicTacToeGui/src/main/java/module-info.java
+++ TicTacToeGui/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.mycompany.tictactoegui {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.mycompany.tictactoegui to javafx.fxml;
     exports com.mycompany.tictactoegui;
