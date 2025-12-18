79a23a40b6ba4aa0cf9eb201643772f24bbb4640
Ahmed Sayed <149809000+ahmedSayed321@users.noreply.github.com>
2025-12-18 22:06:02 +0200

    resolve conflicts

--- TicTacToeGui/src/main/java/com/mycompany/tictactoegui/App.java
+++ TicTacToeGui/src/main/java/com/mycompany/tictactoegui/App.java
@@ -18,6 +18,8 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("signup"), 640, 600);
+
+        scene = new Scene(loadFXML("home"), 640, 480);
         stage.setScene(scene);
         stage.show();
     }
