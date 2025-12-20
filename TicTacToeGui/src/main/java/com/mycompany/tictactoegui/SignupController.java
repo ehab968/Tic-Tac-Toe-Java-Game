8f6dd8c49326b7f5c26476f023f7ce8fa8c7e9fb
ehab968 <e.salah82055@gmail.com>
2025-12-20 06:34:09 +0200

    Refactor SignupController Class
    - convert initalize socket and streams from the initialize method to the handle register method
    - close streams and socket after sucess registeration

--- TicTacToeGui/src/main/java/com/mycompany/tictactoegui/SignupController.java
+++ TicTacToeGui/src/main/java/com/mycompany/tictactoegui/SignupController.java
@@ -28,74 +28,58 @@ public class SignupController implements Initializable {
     private Socket socket;
     private ObjectOutputStream out;
     private ObjectInputStream in;
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        
-        try {
-            
-            socket = new Socket("localhost", 5005);
-            out = new ObjectOutputStream(socket.getOutputStream());
-             out.flush();
-            in = new ObjectInputStream(socket.getInputStream());
 
-        } catch (IOException ex) {
-            System.getLogger(SignupController.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
-            showAlert("Error", "Cannot access to the server");
-        }
-        
-        
-    }    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) { }
 
     @FXML
     private void handleRegister(ActionEvent event) {
         String userName = usernameField.getText();
         String password = passwordField.getText();
         String confirmPassword = confirmPasswordField.getText();
-        
-        
-        if(userName.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()){  
+
+        if (userName.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
             showAlert("Validation Error", "All fields are required");
             return;
         }
-        if(!password.equals(confirmPassword)){
+        if (!password.equals(confirmPassword)) {
             showAlert("Password Validation Error", "Password Doesn't match");
             return;
         }
-        
-        
+
         UserData us = new UserData(userName, password, 0, 0, 0, 0, 0);
-        
+
         try {
+            socket = new Socket("localhost", 5005);
+            out = new ObjectOutputStream(socket.getOutputStream());
+            out.flush();
+            in = new ObjectInputStream(socket.getInputStream());
+            
             out.writeObject(us);
             out.flush();
-            
-            
             Object response = in.readObject();
             Command cmd = (Command) response;
             if (cmd.getType() == CommandType.REGISTER_SUCCESS) {
                 showAlert("Success", "Account created successfully!");
                 clearFields();
-                 App.setRoot("login");
-            }else if(cmd.getType() == CommandType.USERNAME_EXISTS){
+                out.close();
+                in.close();
+                socket.close();
+                App.setRoot("login");
+            } else if (cmd.getType() == CommandType.USERNAME_EXISTS) {
                 showAlert("Error", "Sorry This account is used before");
                 System.out.println("-------------------------------");
                 System.out.println("********************************");
-            } 
-            else {
+            } else {
                 showAlert("Error", "Registration failed");
             }
+            
         } catch (IOException ex) {
             System.getLogger(SignupController.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
         } catch (ClassNotFoundException ex) {
             System.getLogger(SignupController.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
         }
 
-        
-        
-        
-        
-        
     }
 
     @FXML
@@ -105,17 +89,15 @@ public class SignupController implements Initializable {
         } catch (IOException ex) {
             System.getLogger(SignupController.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
         }
-        
+
     }
-    
-    
+
     private void clearFields() {
         usernameField.clear();
         passwordField.clear();
         confirmPasswordField.clear();
     }
-    
-    
+
     private void showAlert(String title, String msg) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle(title);
@@ -123,5 +105,5 @@ public class SignupController implements Initializable {
         alert.setContentText(msg);
         alert.showAndWait();
     }
-    
+
 }
