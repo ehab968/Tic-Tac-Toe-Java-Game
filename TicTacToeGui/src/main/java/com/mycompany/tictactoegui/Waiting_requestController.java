49539f54ace24554d270269b29855e4077e99542
Ahmed Sayed <149809000+ahmedSayed321@users.noreply.github.com>
2025-12-21 17:37:17 +0200

    finish all request ui

---
+++ TicTacToeGui/src/main/java/com/mycompany/tictactoegui/Waiting_requestController.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.mycompany.tictactoegui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import javafx.scene.control.Button;
+/**
+ * FXML Controller class
+ *
+ * @author Ahmed Sayed
+ */
+public class Waiting_requestController implements Initializable {
+
+
+    @FXML
+    private Button cancelInviteBtn;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    @FXML
+    private void cancelInviteAction(ActionEvent event) {
+    }
+
+}
